ea223439bd7bb19cda472946c35393ed57306ffd
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-20 12:43:29 -0700

    create a route for all hotels page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
- 
 });
 
+router.get("/all", function(req,res){
+  res.render('all_hotels', {title : 'All Hotels'});
+});
 module.exports = router;
