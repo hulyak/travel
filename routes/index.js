e588518a187b50dad872d73e99029be12b18d70a
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-22 16:59:39 -0700

    feat: create routes for admin page

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 //require controllers
 const hotelController = require("../controllers/hotelController");
+const hotel = require('../models/hotel');
 
 /* GET home page. */
 router.get('/', hotelController.homePage);
 
 router.get("/all", hotelController.listAllHotels);
 
+//ADMIN Routes:
+router.get('/admin', hotelController.adminPage);
+router.get('/admin/add', hotelController.createHotelGet);
 // add 'next' by adding comma 
 // router.get('/sign-up', hotelController.signUp , hotelController.login);
 // router.get('/log-in', hotelController.login);
