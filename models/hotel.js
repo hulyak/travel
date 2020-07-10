01d251b245ebb265e5f2d7060abdd830f0ea7d1e
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-10 12:14:55 -0700

    feat: add index fields for form

--- models/hotel.js
+++ models/hotel.js
@@ -3,37 +3,42 @@ const mongoose = require('mongoose');
 //schema will match to the data inside of our database 
 //trim to remove whitespaces
 const hotelSchema = new mongoose.Schema({
-  hotel_name : {
-    type : String,
-    required : 'Hotel name is required',
-    max : 32,
-    trim : true
+  hotel_name: {
+    type: String,
+    required: 'Hotel name is required',
+    max: 32,
+    trim: true
   },
-  hotel_description : {
-    type : String,
-    required : 'Hotel description is required',
-    trim : true
+  hotel_description: {
+    type: String,
+    required: 'Hotel description is required',
+    trim: true
   },
-  image : String,
-  star_rating : {
-    type : Number,
-    required : 'Hotel star rating is required',
-    max : 5
+  image: String,
+  star_rating: {
+    type: Number,
+    required: 'Hotel star rating is required',
+    max: 5
   },
-  country : {
+  country: {
     type: String,
     required: 'Country is required',
-    trim : true
+    trim: true
   },
-  cost_per_night : {
-    type :  Number,
-    required : 'Cost per night is required'
+  cost_per_night: {
+    type: Number,
+    required: 'Cost per night is required'
   },
-  available : {
-    type : Boolean,
-    required :"Availability is required"
+  available: {
+    type: Boolean,
+    required: "Availability is required"
   }
 });
 
+//index the fields to search
+hotelSchema.index({
+  hotel_name: 'text',
+  country: 'text'
+})
 //Export model - 'Hotel' is the name for this model
 module.exports = mongoose.model('Hotel', hotelSchema);
